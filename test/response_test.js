f087fcd2e5efcc2c6d5e61773bab9bebf11b9f8a
Aaron Blohowiak <aaron.blohowiak@gmail.com>
2010-09-25 14:28:44 -0700

    supports all verbs, builds responses

--- test/response_test.js
+++ test/response_test.js
@@ -4,11 +4,13 @@ module.exports = test;
 var Response = require('../lib/twilio/response').Response;
 
 test["should produce xml"] = function(assert){
-  str = Response(function(xml){
-    xml.Say("hiii", {a:"hi"}, function(xml){
-      xml.Gather();
+  var str = Response(function(xml){
+    xml.say( {a:"hi"}, function(xml){
+     xml.gather();
     });
   })
 
+
+  console.log(str)
   assert.isDefined(str)
 }
